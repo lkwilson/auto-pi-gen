3217d0344c40e21b939acfd7e40de05cd1d89633
Lucas Wilson <lkwilson96@gmail.com>
2021-07-03 10:57:43 -0600

    Move files around

---
+++ reset
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$BASH_SOURCE")"
+
+git submodule deinit -f pi-gen
+git submodule update --init
--- stagecus/01-set-timezone/00-run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-
-echo "America/Denver" > "${ROOTFS_DIR}/etc/timezone"
-rm "${ROOTFS_DIR}/etc/localtime"
-
-on_chroot << EOF
-dpkg-reconfigure -f noninteractive tzdata
-EOF
